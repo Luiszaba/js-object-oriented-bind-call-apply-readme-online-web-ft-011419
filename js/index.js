884dece89fe0b61fe4fa14e898a5add54064a780
Luis Zabala <luis.zabala076@gmail.com>
2019-05-06 03:59:18 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
 // use this to copy code snippets or use your browser's console
 
-function greet() {
-    console.log(`my name is ${this.name}, hi!`);
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}`);
 }
  
-greet(); // my name is , hi!
+let sally = { name: 'Sally' };
+
+greet.call( sally, 'Terry', 'George');
  
-let person = {
-    name: 'Bob',
-    greet: greet
-};
- 
-person.greet(); // my name is Bob, hi!
