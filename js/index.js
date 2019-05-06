0f4f77280a66833712daa28e6f60b021f138209a
Luis Zabala <luis.zabala076@gmail.com>
2019-05-06 04:00:26 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -8,3 +8,6 @@ let sally = { name: 'Sally' };
 
 greet.call( sally, 'Terry', 'George');
  
+greet.apply(sally, ['Terry', 'George']);
+// Hi Terry and George, my name is Sally!
+// apply takes the first argument and the second is an array of arguments to be passed in the function
